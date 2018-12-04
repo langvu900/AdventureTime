e1e9a635cf02db32c63fb70a58af62fd226f2b09
langvu900 <45390349+langvu900@users.noreply.github.com>
2018-12-04 18:33:47 +0100

    Add files via upload

---
+++ desktop/src/com/umu/adtime/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.umu.adtime.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.umu.adtime.AdTimeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AdTimeGame(), config);
+	}
+}
